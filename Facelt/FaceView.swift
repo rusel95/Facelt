7dee268c7fb6b3a24f2e50834d0143c887fe1722
Admin <ruslanpopesku95@gmail.com>
2017-04-18 14:10:22 +0300

    First commit

---
+++ Facelt/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Facelt
+//
+//  Created by Admin on 18.04.17.
+//  Copyright © 2017 rusel95. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Facelt/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Facelt
+//
+//  Created by Admin on 18.04.17.
+//  Copyright © 2017 rusel95. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+
+}
+
