f3e4895da9e8dccfbaa7f83265ffdc03b53683f6
rusel95 <ruslanpopesku95@gmail.com>
2017-06-09 18:08:34 +0300

    eyes are animated

--- Facelt/EyeView.swift
+++ Facelt/EyeView.swift
@@ -13,7 +13,20 @@ class EyeView: UIView {
     var lineWidth: CGFloat = 5.0 { didSet { setNeedsDisplay() } }
     var color: UIColor = UIColor.blue { didSet { setNeedsDisplay() } }
     
-    var eyesOpen: Bool = true { didSet { setNeedsDisplay() } }
+    var _eyesOpen: Bool = true { didSet { setNeedsDisplay() } }
+    
+    var eyesOpen: Bool {
+        get {
+            return _eyesOpen
+        }
+        set {
+            if newValue != _eyesOpen {
+                UIView.transition(with: self, duration: 0.4, options: [.transitionFlipFromTop], animations: { 
+                    self._eyesOpen = newValue
+                })
+            }
+        }
+    }
     
     override func draw(_ rect: CGRect) {
         var path: UIBezierPath
